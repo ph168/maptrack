2c04fd67114766333eaff88542e429c8511cc7bc
Peter Hoffmann <p_hoffmann@rocketmail.com>
2013-03-29 15:12:00 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MapTrack::Application.config.secret_token = '3144171ca974a2ddf3c4efaaccdf47e7b0c7ceecef866281e3a2d6b2de4e82c7b2f6a597cba1642235559855b53e479e592a68aa7dbb7929e00a2720f935984a'
