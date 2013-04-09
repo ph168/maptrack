95ed5750fa37b0cd958c0034c4c9c856913fbc0d
Peter Hoffmann <p_hoffmann@rocketmail.com>
2013-04-09 02:44:12 +0200

    formatting

--- app/helpers/mapi.rb
+++ app/helpers/mapi.rb
@@ -1,23 +1,23 @@
 module Mapi
-	def mapi_coordinate(coordinate)
-		output = mapi_begin
-		output += "var map = new Mapi.Map({div:'map'});\n" +
-		"var pos = new Mapi.Loc(#{coordinate.latitude}, #{coordinate.longitude});" +
-		"map.setCenter(pos, 16);\n" +
-		"var marker = new Mapi.Marker(pos);\n" +
-		"map.addMarker(marker);\n"
-		output += mapi_end
-	end
+  def mapi_coordinate(coordinate)
+    output = mapi_begin
+    output += "var map = new Mapi.Map({div:'map'});\n" +
+    "var pos = new Mapi.Loc(#{coordinate.latitude}, #{coordinate.longitude});" +
+    "map.setCenter(pos, 16);\n" +
+    "var marker = new Mapi.Marker(pos);\n" +
+    "map.addMarker(marker);\n"
+    output += mapi_end
+  end
 
-	private
+  private
 
-	def mapi_begin
-		"<div id='map' style='width:800px; height:450px;'> </div>" +
-		"<script type='text/javascript'>" +
-		"Mapi.Load({dir:'/assets/mapi/'}, function() {\n"
-	end
+  def mapi_begin
+    "<div id='map' style='width:800px; height:450px;'> </div>" +
+    "<script type='text/javascript'>" +
+    "Mapi.Load({dir:'/assets/mapi/'}, function() {\n"
+  end
 	
-	def mapi_end
-		"});</script>"
-	end
+  def mapi_end
+    "});</script>"
+  end
 end
