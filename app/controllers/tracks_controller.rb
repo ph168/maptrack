954394dc83b6d73d1e8bfcdba14ae63c19e71407
Peter Hoffmann <p_hoffmann@rocketmail.com>
2013-04-10 22:53:51 +0200

    implemented gpx output for tracks

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -18,6 +18,7 @@ class TracksController < ApplicationController
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @track }
+      format.xml
     end
   end
 
