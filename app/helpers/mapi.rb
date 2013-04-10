045829bb0c1a1ca2fa4db85ab40604e06ef58aad
Peter Hoffmann <p_hoffmann@rocketmail.com>
2013-04-10 23:29:36 +0200

    implemented (map) views

--- app/helpers/mapi.rb
+++ app/helpers/mapi.rb
@@ -1,11 +1,22 @@
 module Mapi
   def mapi_coordinate(coordinate)
     output = mapi_begin
-    output += "var map = new Mapi.Map({div:'map'});\n" +
-    "var pos = new Mapi.Loc(#{coordinate.latitude}, #{coordinate.longitude});" +
+    output += "var pos = new Mapi.Loc(#{coordinate.latitude}, #{coordinate.longitude});" +
     "map.setCenter(pos, 16);\n" +
     "var marker = new Mapi.Marker(pos);\n" +
     "map.addMarker(marker);\n"
+    output += mapi_end
+  end
+
+  def mapi_track(coordinates)
+    output = mapi_begin
+    output += "var coords = new Array();"
+    i = 0;
+    coordinates.each {|c|
+      output += "coords[#{i+=1}] = new Mapi.Loc(#{c.latitude}, #{c.longitude});\n"
+    }
+    output += "var route = new Mapi.Route({points: coords});"
+    output += "route.show()"
     output += mapi_end
   end
 
@@ -14,7 +25,8 @@ module Mapi
   def mapi_begin
     "<div id='map' style='width:800px; height:450px;'> </div>" +
     "<script type='text/javascript'>" +
-    "Mapi.Load({dir:'/assets/mapi/'}, function() {\n"
+    "Mapi.Load({dir:'/assets/mapi/'}, function() {\n" +
+    "var map = new Mapi.Map({div:'map'});\n"
   end
 	
   def mapi_end
--- app/helpers/tracks_helper.rb
+++ app/helpers/tracks_helper.rb
@@ -1,2 +1,3 @@
 module TracksHelper
+  include Mapi
 end
