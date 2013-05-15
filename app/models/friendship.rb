3f9e38f2a370e350e5fa117583564a35fe8f3b36
Peter Hoffmann <p_hoffmann@rocketmail.com>
2013-05-15 09:55:58 +0200

    preventing self-friendship

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -2,7 +2,15 @@ class Friendship < ActiveRecord::Base
   attr_accessible :consumer_id, :confirmed 
   belongs_to :initiator, :class_name => "User"
   belongs_to :consumer, :class_name => "User"
-  
+
+  validate :no_self_friendship
+
+  def no_self_friendship
+    if consumer_id == initiator_id
+      errors.add("You cannot be a friend of yourself.")
+    end
+  end
+
   def users
     [initiator, consumer]
   end
