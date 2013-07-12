658f706619af3621ee44b33be767ad1bff7fda5c
Peter Hoffmann <p_hoffmann@rocketmail.com>
2013-07-12 12:55:22 -0400

    set root to tracks#index

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ MapTrack::Application.routes.draw do
   
   devise_for :users, :path => "auth", :path_names => { :sign_in => 'login', :sign_out => 'logout', :password => 'secret', :confirmation => 'verification', :unlock => 'unblock', :registration => 'register', :sign_up => 'cmon_let_me_in' }
 
-  root :to => "home#index"
+  root :to => "tracks#index"
 
   resources :tracks do
     resources :coordinates
