4a97339884acb7b29df891ad2e8d997413eb3b4c
Peter Hoffmann <p_hoffmann@rocketmail.com>
2013-08-18 21:27:03 +0200

    better map integration

--- app/assets/javascripts/mapview.js
+++ app/assets/javascripts/mapview.js
@@ -1,6 +1,6 @@
-var MapView = function() {
+var MapView = function(varName) {
 
-	this.map = new OpenLayers.Map ({
+	map = new OpenLayers.Map ({
 		controls:[
 			new OpenLayers.Control.Navigation(),
 			new OpenLayers.Control.PanZoomBar(),
@@ -15,23 +15,55 @@ var MapView = function() {
 	});
 
 	layerMapnik = new OpenLayers.Layer.OSM.Mapnik("Mapnik");
-	this.map.addLayer(layerMapnik);
-
-	//layerCycleMap = new OpenLayers.Layer.OSM.CycleMap("CycleMap");
-	//this.map.addLayer(layerCycleMap);
+	map.addLayer(layerMapnik);
 
 	layerMarkers = new OpenLayers.Layer.Markers("Current position");
-	this.map.addLayer(layerMarkers);
+	map.addLayer(layerMarkers);
 
-	this.setContainer = function(div) {
-		this.map.render(div);
+	this.setMapContainer = function(id) {
+		map.render(id);
 	}
 
-	this.addGPXLayer = function(gpxURL, name) {
+	this.updateSize = function() {
+		map.updateSize();
+	}
+
+	trackList = null;
+	trackList_liClass = null;
+
+	this.setMenuContainer = function(id, ulClass, liClass) {
+		var menuContainer = $("#"+id);
+
+		trackList = $(document.createElement("ul"));
+		trackList.addClass(ulClass);
+		menuContainer.append(trackList);
+		trackList_liClass = liClass;
+	}
+
+	tracks = new Array();
+	liveViewTrack = null;
+	colors = ["red", "blue", "green", "orange", "purple", "yellow"];
+	nextColorIdx = 0;
+
+	this.addTrack = function(gpxURL, name) {
+
+		var duplicate = false;
+		$.each(tracks, function(key, track) {
+			if (gpxURL == track.protocol.url) {
+				duplicate = true;
+				return false;
+			}
+		});
+		if (duplicate)
+			return;
+
 		var refreshStrategy = new OpenLayers.Strategy.Refresh({
 			interval: 2000,
 			force: true
 		});
+
+		var color = colors[(nextColorIdx++) % colors.length];
+
 		var lgpx = new OpenLayers.Layer.Vector(name, {
 			strategies: [
 				new OpenLayers.Strategy.Fixed(),
@@ -40,21 +72,96 @@ var MapView = function() {
 				url: gpxURL,
 				format: new OpenLayers.Format.GPX()
 			}),
-			style: {strokeColor: "green", strokeWidth: 5, strokeOpacity: 0.5},
+			style: {strokeColor: color, strokeWidth: 5, strokeOpacity: 0.5},
 			projection: new OpenLayers.Projection("EPSG:4326")
 		});
 
-		// Live view
-		/*lgpx.events.register("featureadded", lgpx, function(f) {
-			var components = f.feature.geometry.components;
-			this.map.setCenter(components[components.length - 1].getBounds().getCenterLonLat(), this.map.getZoom());
-		});*/
-		// Full view
-		/*lgpx.events.register("changelayer", lgpx, function(f) {
-			this.map.zoomToExtent(lgpx.getDataExtent());
-		});*/
-
-		this.map.addLayer(lgpx);
-		//this.map.zoomToExtent(lgpx.getDataExtent());
+		tracks.push(lgpx);
+		map.addLayer(lgpx);
+		if (liveViewTrack == null)
+			setLiveView(lgpx);
+		updateTrackList();
 	}
+
+	this.removeTrack = function(gpxURL) {
+		$.each(tracks, function(key, track) {
+			if (gpxURL == track.protocol.url) {
+				tracks.splice(key, 1);
+				map.removeLayer(track);
+				if (track == liveViewTrack)
+					setLiveView(tracks[0]);
+				return false;
+			}
+		});
+		updateTrackList();
+	}
+
+	this.liveViewFor = function(gpxURL) {
+		var changed = false;
+		$.each(tracks, function(key, track) {
+			if (gpxURL == track.protocol.url) {
+				setLiveView(track);
+				changed = true;
+				return false;
+			}
+		});
+		if (!changed) {
+			setLiveView(null);
+		}
+		updateTrackList();
+	}
+
+	doLiveView = function(f) {
+		var components = f.feature.geometry.components;
+		map.setCenter(components[components.length - 1].getBounds().getCenterLonLat(), map.getZoom());
+	}
+
+	function setLiveView(track) {
+		if (liveViewTrack != null) {
+			liveViewTrack.events.unregister("featureadded", liveViewTrack, doLiveView);
+		}
+		if (track == null) {
+			liveViewTrack = null;
+		} else {
+			track.events.register("featureadded", track, doLiveView);
+			liveViewTrack = track;
+		}
+	}
+
+	function updateTrackList() {
+		trackList.text("");
+		$.each(tracks, function(key, track) {
+			var li = $(document.createElement("li"));
+			li.addClass(trackList_liClass + " background-" + track.style.strokeColor);
+			if (track == liveViewTrack) {
+				li.append(
+					$(document.createElement("span"))
+					.addClass("icon icon-magnet")
+					.attr("title", "Live view active, click to disable.")
+					.attr("onClick", varName + ".liveViewFor(null)")
+					.css("cursor", "pointer")
+				);
+			} else {
+				li.append(
+					$(document.createElement("span"))
+					.addClass("icon icon-stop")
+					.attr("title", "Live view not active, click to enable.")
+					.attr("onClick", varName + ".liveViewFor('" + track.protocol.url + "')")
+					.css("cursor", "pointer")
+				);
+			}
+			li.append(
+				$(document.createElement("span"))
+				.text(track.name)
+			);
+			li.append(
+				$(document.createElement("span"))
+				.addClass("icon icon-remove")
+				.attr("onClick", varName + ".removeTrack('" + track.protocol.url + "')")
+				.css("cursor", "pointer")
+			);
+			trackList.append(li);
+		});
+	}
+
 }
