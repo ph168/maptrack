3dc31e946f0f518eb424c435ff4cebf2d472d51c
Peter Hoffmann <p_hoffmann@rocketmail.com>
2013-08-21 23:28:43 +0200

    tests for UsersController

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -1,7 +1,45 @@
 require 'test_helper'
 
-class UserControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+class UsersControllerTest < ActionController::TestCase
+  setup do
+    @user = users(:one)
+    @user2 = users(:two)
+    @friendship = friendships(:one)
+    sign_in @user
+  end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_equal @user, assigns(:user)
+  end
+
+  test "should show user" do
+    get :show, :format => "js", name: @user2.name
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get :edit, :format => "js"
+    assert_response :success
+    assert_equal @user, assigns(:user)
+  end
+
+  test "should update user" do
+    put :update, user: {  }
+    assert_response :success
+  end
+
+  test "should request friendship" do
+    assert_difference('Friendship.count') do
+      post :request_friendship, friendship: { consumer_id: @user2.id }
+    end
+
+    assert_redirected_to action: "index", notice: "Friendship was successfully requested."
+  end
+
+  test "should confirm friendship" do
+    post :confirm_friendship, id: @friendship.id
+    assert_redirected_to action: "index", notice: "Friendship was successfully confirmed."
+  end
 end
