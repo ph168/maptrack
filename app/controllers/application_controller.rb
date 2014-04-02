2834a878c691f6ab42f52d1813693dc246ac0c43
Peter Hoffmann <hoffmann.p@gmx.at>
2014-04-02 16:49:44 +0200

    enable http_authenticatable and send csrf token as header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  after_filter :csrf
+
+  def csrf
+    response.header['X-CSRF-Token'] = form_authenticity_token
+  end
 end
