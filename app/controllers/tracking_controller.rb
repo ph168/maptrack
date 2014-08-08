8f611e02cf5f22203a47e6174e959855f5f4e167
Peter Hoffmann <hoffmann.p@gmx.at>
2014-08-08 09:48:56 +0200

    add controller for simple tracking applications

---
+++ app/controllers/tracking_controller.rb
@@ -0,0 +1,32 @@
+# Controller used for simple tracking with GET requests
+
+class TrackingController < ApplicationController
+  before_filter :authenticate_with_token
+  before_filter :find_track
+
+  def track
+    coordinate = @track.coordinates.build params
+    coordinate.user_id = @user.id
+    if coordinate.save
+      head :ok
+    else
+      head :unprocessable_entity
+    end
+  end
+
+  private
+
+  def authenticate_with_token
+    @user = User.find_by_token params[:token]
+    head :unauthorized unless @user
+  end
+
+  def find_track
+    @track = Track.find_by_name params[:track]
+    unless @track
+      @track = Track.new :name => params[:track]
+      @track.user = @user
+      head :unprocessable_entity unless @track.save
+    end
+  end
+end
